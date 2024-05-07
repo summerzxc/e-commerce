807e0280f29f0696ba180aa74c2376788023a602
summerzxc <cell.unobtainable.88@gmail.com>
2024-05-07 11:29:04 +0800

    Bug Fix

--- pages/components/_molecule/Navbar.jsx
+++ pages/components/_molecule/Navbar.jsx
@@ -2,21 +2,22 @@ import React from "react";
 import { RiShoppingBagFill } from "react-icons/ri";
 import Menu from "../menu/Menu";
 import Cart from "../shop_cart/Cart";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="w-full flex justify-between items-center relative py-[30px]">
       <nav className="flex items-center gap-3 text-[14px]">
-        <a href="/shop">SHOP</a>
-        <a href="/bundles">BUNDLES</a>
+        <Link href="/shop">SHOP</Link>
+        <Link href="/bundles">BUNDLES</Link>
         <Menu />
       </nav>
-      <a
+      <Link
         href="/"
         className="text-5xl absolute left-[50%] transform -translate-x-1/2 tracking-tight bebas"
       >
         soleil
-      </a>
+      </Link>
       <Cart />
     </div>
   );
